36044d1829cff8e751318013393f541029d38433
luisop <luisfop77@gmail.com>
2020-09-10 17:16:16 -0300

    settle a small and simple application to apply the context study

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React from "react";
 
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
-
-import About from './pages/about';
-import Index from './pages';
-
-
+import About from "./pages/about";
+import Index from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-
-      <About/>
-      <Index/>
-
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/about/" component={About}/>
+        <Route path="/" exact component={Index}/>
+      </div>
+    </Router>
   );
 }
 
